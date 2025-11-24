c529e7d0b2e7fddf3c750a6ff289d1e1de52f3c2
Veronique Phan <vephan@c1r16s5.42lausanne.ch>
2025-11-24 15:44:50 +0100

    add push_swap

---
+++ common-core/rank2/push_swap/push_swap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vephan <vephan@student.42lausanne.ch>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 15:03:36 by vephan            #+#    #+#             */
+/*   Updated: 2025/11/24 15:04:03 by vephan           ###   ########.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* push swap */
+/* input: stack a - list of integers */
+// a1, a2, a3, ...
+// | a1 |
+// | a2 |
+// | a3 |
+/* output: displays the list of instructions needed
+ to sort stack a with smallest number at the top */
+/* variables:*/
+// list_a
+// list_ops
+// cnt - number of instructions
+// maxops = max operations allowed
+// a_size = nb of arguments in list a
+/* Errors - checking arguments*/
+// Arguments duplicate!
+// Arguments outside of MAX_INT, MIN_INT
+// Arguments not integers ()
+
